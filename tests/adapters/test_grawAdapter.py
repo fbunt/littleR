891673e2a17a9c3aa6b6d7ef0f08b8b09fe4942c
Fred Bunt <Fred@My-MacBook-Pro.local>
2016-12-02 19:55:20 -0700

    Add failing test for GrawAdapter’s count

--- tests/adapters/test_grawAdapter.py
+++ tests/adapters/test_grawAdapter.py
@@ -1,5 +1,17 @@
 from unittest import TestCase
 
+import os
+
+from littler.adapters import GrawAdapter
+
 
 class TestGrawAdapter(TestCase):
-    pass
+
+    def setUp(self):
+        with open('../data/GrawProfile_7_14_SLU.txt', encoding='latin-1') as fd:
+            self.adapter = GrawAdapter(fd)
+
+    def test_count(self):
+        self.assertEqual(self.adapter.count, 4040)
+
+
