a79d0516d2f9113340afb450f777ad787573d5c9
Fred Bunt <Fred@My-MacBook-Pro.local>
2016-12-07 16:00:12 -0700

    Add test for GrawAdapter.levels

--- tests/adapters/test_grawAdapter.py
+++ tests/adapters/test_grawAdapter.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 
 import os
 
-from littler.adapters import GrawAdapter
+from littler.adapters import GrawAdapter, Level
+
+test_file_count = 4040
 
 
 class TestGrawAdapter(TestCase):
@@ -12,6 +14,11 @@ class TestGrawAdapter(TestCase):
             self.adapter = GrawAdapter(fd)
 
     def test_count(self):
-        self.assertEqual(self.adapter.count, 4040)
+        self.assertEqual(self.adapter.count, test_file_count, 'adapter.count must equal data\'s count')
 
+    def test_levels(self):
+        self.assertEqual(len(self.adapter.levels), test_file_count, 'adapter.levels must be same length as data')
+        self.assertEqual(len(self.adapter.levels), self.adapter.count, 'len(adapter.levels) must equal adapter.count')
+        self.assertTrue(all(isinstance(x, Level) for x in self.adapter.levels),
+                        'All elements in adapter.levels must be a Level')
 
