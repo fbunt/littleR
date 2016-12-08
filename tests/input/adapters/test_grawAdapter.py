462008c7d38bedfb2a25f58349e5cb9393505314
Fred Bunt <Fred@My-MacBook-Pro.local>
2016-12-08 16:54:00 -0700

    Add test for date formatting

--- tests/input/adapters/test_grawAdapter.py
+++ tests/input/adapters/test_grawAdapter.py
@@ -12,7 +12,8 @@ class TestGrawAdapter(TestCase):
 
     def setUp(self):
         with open('../../data/GrawProfile_7_14_SLU.txt', encoding='latin-1') as fd:
-            self.adapter = GrawAdapter(fd, datetime.datetime.today())
+            self.now = datetime.datetime.today()
+            self.adapter = GrawAdapter(fd, self.now)
 
     def test_count(self):
         self.assertEqual(self.adapter.count, test_file_count, 'adapter.count must equal data\'s count')
@@ -22,4 +23,8 @@ class TestGrawAdapter(TestCase):
         self.assertEqual(len(self.adapter.levels), self.adapter.count, 'len(adapter.levels) must equal adapter.count')
         self.assertTrue(all(isinstance(x, Level) for x in self.adapter.levels),
                         'All elements in adapter.levels must be a Level')
+
+    def test_time_string(self):
+        test_str = self.now.strftime('%Y%M%d%H%m%S')
+        self.assertEqual(self.adapter.getlevel(0).date, test_str, 'Data string must match expected format')
 
