2f555b3abff055d33be0c68cdd53d9d95d86142d
Fred Bunt <Fred@wl-65-24.wireless.umt.edu>
2016-12-19 18:56:30 -0700

    update record test

--- tests/output/test_littleROut.py
+++ tests/output/test_littleROut.py
@@ -10,6 +10,15 @@ class TestLittleROut(TestCase):
         lv = Level()
         rec = _Record(lv)
         expected_str = '-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0'
-        self.assertTrue(str(rec) == expected_str, "Default Record str must match expected")
+        self.assertEqual(str(rec), expected_str, "Default Record str must match expected")
+
+        lv = Level()
+        lv.pres = (83500.00000, 0)
+        lv.temp = (264.44998, 0)
+        lv.dewpoint = (263.35001, 0)
+        rec = _Record(lv)
+        expected_str = '  83500.00000      0-888888.00000      0    264.44998      0    263.35001      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0-888888.00000      0'
+        self.assertEqual(str(rec), expected_str, "Record str must match expected")
+
 
 
