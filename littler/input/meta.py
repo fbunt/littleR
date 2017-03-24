b7b78c871b97400a611a81d09b5bc501de1b6a9b
Fred Bunt <Fred@wl-64-103.wireless.umt.edu>
2017-03-24 17:04:13 -0600

    Let defaults override meta data in file header

--- littler/input/meta.py
+++ littler/input/meta.py
@@ -10,7 +10,19 @@ class MetaDataParsingError(LittleRError):
     pass
 
 
-def parse_meta_data(fd):
+_ID_KEY = 'id'
+_SOURCE_KEY = 'source'
+_DATE_KEY = 'date'
+_START_TIME_KEY = 'start time'
+
+
+def parse_meta_data(fd, default_id=None, default_source=None,
+                    default_name=None, default_datetime=None):
+    """Parse the meta information for the data from the file
+
+    If the defaults are specified, they will override the corresponding
+    values in the files header.
+    """
     line = fd.readline()
     if not line.strip() == '---':
         raise MetaDataParsingError('Could not find meta data')
@@ -26,14 +38,20 @@ def parse_meta_data(fd):
         raise MetaDataParsingError('Failed while attempting to parse meta data')
 
     try:
-        id_ = data['id']
-        source = data['source']
-        date = _parse_date(data['date'])
-        time = _parse_time(data['start time'])
+        id_ = default_id if default_id is not None else data[_ID_KEY]
+        source = default_source if default_id is not None else data[_SOURCE_KEY]
+        if default_datetime is None:
+            date = _parse_date(data[_DATE_KEY])
+            time = _parse_time(data[_START_TIME_KEY])
+            dt = datetime.datetime.combine(date, time)
+        else:
+            dt = default_datetime
     except KeyError:
         raise MetaDataParsingError('Invalid header title(s)')
-    dt = datetime.datetime.combine(date, time)
-    name = os.path.splitext(os.path.basename(fd.name))[0]
+    if default_name is None:
+        name = name = os.path.splitext(os.path.basename(fd.name))[0]
+    else:
+        name = default_name
     return id_, source, name, dt
 
 
